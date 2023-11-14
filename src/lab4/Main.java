0e24a633be0fda217331e23b1c061a28bb25d82b
polainait <polaina.mezentseva@mail.ru>
2023-11-15 02:58:26 +0300

    Minimum working option ;(

--- src/lab4/Main.java
+++ src/lab4/Main.java
@@ -1,9 +1,7 @@
 package lab4;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Pattern;
 
 import static java.io.File.separator;
@@ -16,15 +14,14 @@ public class Main {
     //Должно все переводится без изменения исх кода
     //обработать исключения + создать свои
 
-    public static final String DELIMITER = "[\\s,\\.\\n]";
-    public static final String DELIMITER_FOR_DICTIONARY = "[\\s*\\|\\s*]";
+    public static final String DELIMITER = "[\\s,\\n]";
 
     public static void main(String[] args) throws IOException { //исправить
         Map<String, String> map = new HashMap<>();
         File file = new File("src" + separator + "lab4" + separator + "resources" + separator + "dictionary.txt");
         fillDictionary(map, file);
 
-        file = new File("src" + separator + "lab4" + separator + "resources" + separator + "input2.txt");
+        file = new File("src" + separator + "lab4" + separator + "resources" + separator + "input1.txt");
         translate(map, file);
     }
 
@@ -32,7 +29,7 @@ public class Main {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(DELIMITER_FOR_DICTIONARY);
+                String[] parts = line.split("\\s*\\|\\s*");
                 if (parts.length == 2) {
                     map.put(parts[0], parts[1]);
                 }
@@ -44,13 +41,56 @@ public class Main {
         Scanner scanner = new Scanner(file);
         Pattern pattern = Pattern.compile(DELIMITER);
         scanner.useDelimiter(pattern);
-
-        String tempWord, word;
+        StringBuilder word = new StringBuilder();
+        ArrayList<String> translationOptions = new ArrayList<>();
         while (scanner.hasNext()) {
-            word = scanner.next();
-            tempWord = word.toLowerCase();
-
-            System.out.println(map.getOrDefault(tempWord, word));
+            word.append(scanner.next().toLowerCase());
+            if (String.valueOf(word).equals("")) {
+                System.out.println();
+            }
+            for (String str: map.keySet()) {
+                if (str.startsWith(String.valueOf(word))) {
+                    translationOptions.add(str);
+                }
+            }
+            if (translationOptions.size() == 0) {
+                System.out.print(word + " ");
+                word.delete(0, word.length());
+            }
+            else if (translationOptions.size() == 1 && translationOptions.get(0).equals(String.valueOf(word))) {
+                System.out.print(map.get(String.valueOf(word)) + " ");
+                translationOptions.clear();
+                word.delete(0, word.length());
+            }
+            else {
+                while (scanner.hasNext() && !word.isEmpty()) {
+                    word.append(" ");
+                    word.append(scanner.next().toLowerCase());
+                    ArrayList<String> tempTranslationOptions = new ArrayList<>(translationOptions);
+                    for (String str:tempTranslationOptions) {
+                        if (!str.startsWith(String.valueOf(word))) {
+                            translationOptions.remove(str);
+                        }
+                    }
+                    if (translationOptions.size() == 0) {
+                        if (map.get(String.valueOf(word)) != null) {
+                            System.out.println(map.get(String.valueOf(word)));
+                        }
+                        else {
+                            String[] parts = String.valueOf(word).split("\\s");
+                            for (String str:parts) {
+                                System.out.print(map.getOrDefault(str, str) + " ");
+                            }
+                        }
+                        word.delete(0, word.length());
+                    }
+                    else if (translationOptions.size() == 1 && translationOptions.get(0).equals(String.valueOf(word))) {
+                        System.out.print(map.get(String.valueOf(word)) + " ");
+                        word.delete(0, word.length());
+                    }
+                }
+                translationOptions.clear();
+            }
         }
     }
 }
